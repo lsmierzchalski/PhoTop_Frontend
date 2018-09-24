4e9462738c4201cb611ec2ba7a276b720df75c9d
lsmierzchalski <lsmierzchalski@gmail.com>
2018-09-24 19:40:09 +0200

    clera comments from httpservice

--- PhoTop/src/app/_services/http.service.ts
+++ PhoTop/src/app/_services/http.service.ts
@@ -12,34 +12,6 @@ export class HttpService {
 
     constructor(private http: HttpClient) { }
 
-    // getPhotos(): Observable<Array<any>> {
-    // tslint:disable-next-line:max-line-length
-    //     const auth_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYWNla3BrIiwiQWRtaW4iOjI0LCJleHAiOjE1MzgyMTczMDR9.NVA05SikeOS19W3w619NTi-IiAw44h1UpvapbSNgxmpE3zFOCxcUCG2pu1Kda-wZxRmvHQloBXuq5cw5hWPIXQ';
-    //     const headers = new Headers({
-    //     'Content-Type': 'application/json',
-    //     'Authorization': auth_token
-    //     })
-    //     return this.http.get<Array<any>>(`${environment.photopApiUrl}/photos`, { headers });
-    // }
-
-    // getPhoto(): Observable<any> {
-        // tslint:disable-next-line:max-line-length
-    //     const auth_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYWNla3BrIiwiQWRtaW4iOjI0LCJleHAiOjE1MzgyMTczMDR9.NVA05SikeOS19W3w619NTi-IiAw44h1UpvapbSNgxmpE3zFOCxcUCG2pu1Kda-wZxRmvHQloBXuq5cw5hWPIXQ';
-    //     const headers = new Headers({
-    //     'Content-Type': 'application/json',
-    //     'Authorization': auth_token
-    //     });
-    //     return this.http.get<Array<any>>(`${environment.photopApiUrl}/photos/1`, { headers });
-    // }
-
-    // getPhoto(): string {
-    //     const myHeaders = new Headers();
-    // tslint:disable-next-line:max-line-length
-    //     myHeaders.append('Authorization', 'Bearer Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYWNla3BrIiwiQWRtaW4iOjI0LCJleHAiOjE1MzgyMTczMDR9.NVA05SikeOS19W3w619NTi-IiAw44h1UpvapbSNgxmpE3zFOCxcUCG2pu1Kda-wZxRmvHQloBXuq5cw5hWPIXQ');
-
-    //     return this.http.get(`${environment.photopApiUrl}/photos/1`);
-    // }
-
     getPhotos(): Observable<Photo[]> {
         return this.http.get<Photo[]>('http://localhost:8090/photos');
     }
