9ee6a65152a68f5825ccda053de6b41912828334
lsmierzchalski <lsmierzchalski@gmail.com>
2018-10-05 23:24:51 +0200

    change design seach date page and logic

---
+++ PhoTop/src/app/_models/select_date.ts
@@ -0,0 +1,8 @@
+import {NgbDateStruct, NgbCalendar, NgbDate, NgbDatepickerI18n, NgbCalendarPersian} from '@ng-bootstrap/ng-bootstrap';
+
+export * from './select_date';
+
+export class SelectDate {
+    fromDate: NgbDate;
+    toDate: NgbDate;
+}
--- PhoTop/src/app/search-by-date-page/search-by-date-page.component.ts
+++ PhoTop/src/app/search-by-date-page/search-by-date-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Injectable} from '@angular/core';
-import {NgbDateStruct, NgbCalendar, NgbDate, NgbDatepickerI18n, NgbCalendarPersian} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { NgbDateStruct, NgbCalendar, NgbDate, NgbDatepickerI18n, NgbCalendarPersian } from '@ng-bootstrap/ng-bootstrap';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { HttpService } from '../_services/http.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Photo } from '../_models/photo';
+import { SelectDate } from '../_models/select_date';
 
 const WEEKDAYS_SHORT = ['Pon', 'Wt', 'Śr', 'Czw', 'Pt', 'Sob', 'Niedz'];
 const MONTHS = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec',
@@ -12,38 +13,49 @@ const MONTHS = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec',
 
 @Injectable()
 export class NgbDatepickerI18nPersian extends NgbDatepickerI18n {
-  getWeekdayShortName(weekday: number) { return WEEKDAYS_SHORT[weekday - 1]; }
-  getMonthShortName(month: number) { return MONTHS[month - 1]; }
-  getMonthFullName(month: number) { return MONTHS[month - 1]; }
-  getDayAriaLabel(date: NgbDateStruct): string { return `${date.year}-${this.getMonthFullName(date.month)}-${date.day}`; }
+    getWeekdayShortName(weekday: number) { return WEEKDAYS_SHORT[weekday - 1]; }
+    getMonthShortName(month: number) { return MONTHS[month - 1].substring(0, 3); }
+    getMonthFullName(month: number) { return MONTHS[month - 1]; }
+    getDayAriaLabel(date: NgbDateStruct): string { return `${date.year}-${this.getMonthFullName(date.month)}-${date.day}`; }
 }
 @Component({
     selector: 'app-search-by-date-page',
     templateUrl: 'search-by-date-page.component.html',
     styleUrls: ['search-by-date-page.component.css'],
     providers: [
-        {provide: NgbDatepickerI18n, useClass: NgbDatepickerI18nPersian}
+        { provide: NgbDatepickerI18n, useClass: NgbDatepickerI18nPersian }
     ]
 })
 export class SearchByDatePageComponent implements OnInit {
 
     photos: Photo[] = [];
 
+    modelFromDate;
+    modelToDate;
     model: NgbDateStruct;
-    date: {year: number, month: number};
+    date: { year: number, month: number };
 
     hoveredDate: NgbDate;
 
     fromDate: NgbDate;
     toDate: NgbDate;
 
+    placeholderDate = 'Od rrrr-mm-dd Do rrrr-mm-dd';
+
     constructor(private httpSerive: HttpService, calendar: NgbCalendar, private router: Router) {
-        this.fromDate = calendar.getToday();
-        this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+        const selectDate = JSON.parse(localStorage.getItem('selectDate'));
+        if (selectDate) {
+            this.fromDate = selectDate.fromDate;
+            this.toDate = selectDate.toDate;
+        } else {
+            this.fromDate = calendar.getPrev(calendar.getToday(), 'd', 10);
+            this.toDate = calendar.getToday();
+        }
     }
 
     ngOnInit() {
         this.loadPhotosByDate();
+        this.changePlaceholder();
     }
 
     onDateSelection(date: NgbDate) {
@@ -58,7 +70,13 @@ export class SearchByDatePageComponent implements OnInit {
 
         if (this.toDate != null && this.fromDate != null) {
             this.loadPhotosByDate();
+            this.changePlaceholder();
         }
+    }
+
+    private changePlaceholder() {
+        // tslint:disable-next-line:max-line-length
+        this.placeholderDate = `Od ${this.fromDate.year}-${this.fromDate.month}-${this.fromDate.day} Do ${this.toDate.year}-${this.toDate.month}-${this.toDate.day}`;
     }
 
     isHovered(date: NgbDate) {
@@ -84,6 +102,10 @@ export class SearchByDatePageComponent implements OnInit {
         selectPhoto.photo_id = photo_id;
         localStorage.setItem('selectPhoto', JSON.stringify(selectPhoto));
 
+        const selectDate = new SelectDate();
+        selectDate.fromDate = this.fromDate;
+        selectDate.toDate = this.toDate;
+        localStorage.setItem('selectDate', JSON.stringify(selectDate));
         this.router.navigateByUrl('/wybrane-zdjecie');
     }
 }
